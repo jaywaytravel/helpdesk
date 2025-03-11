eb9508dccdcb54d3e25bd0962649b0fce09c96a8
chekhotskiy <alexey@jaywaytravel.com>
2025-03-11 15:13:11 +0100

    add logs if group was updated

--- src/controllers/api/v1/groups.js
+++ src/controllers/api/v1/groups.js
@@ -12,12 +12,13 @@
  *  Copyright (c) 2014-2019. All rights reserved.
  */
 
-var _ = require('lodash')
-var async = require('async')
-var GroupSchema = require('../../../models/group')
-var ticketSchema = require('../../../models/ticket')
+const _ = require('lodash')
+const async = require('async')
+const GroupSchema = require('../../../models/group')
+const ticketSchema = require('../../../models/ticket')
+const winston = require('./../../../logger')
 
-var apiGroups = {}
+const apiGroups = {}
 
 /**
  * @api {get} /api/v1/groups Get Groups
@@ -38,9 +39,9 @@ var apiGroups = {}
  *
  */
 apiGroups.get = function (req, res) {
-  var user = req.user
-  var permissions = require('../../../permissions')
-  var hasPublic = permissions.canThis(user.role, 'tickets:public')
+  const user = req.user
+  const permissions = require('../../../permissions')
+  const hasPublic = permissions.canThis(user.role, 'tickets:public')
 
   if (user.role.isAgent || user.role.isAdmin) {
     GroupSchema.getAllGroups(function (err, groups) {
@@ -51,7 +52,7 @@ apiGroups.get = function (req, res) {
           return !g.public
         })
 
-      return res.json({ success: true, groups: groups })
+      return res.json({ success: true, groups })
     })
   } else {
     GroupSchema.getAllGroupsOfUser(user._id, function (err, groups) {
@@ -63,10 +64,10 @@ apiGroups.get = function (req, res) {
 
           groups = groups.concat(grps)
 
-          return res.json({ success: true, groups: groups })
+          return res.json({ success: true, groups })
         })
       } else {
-        return res.json({ success: true, groups: groups })
+        return res.json({ success: true, groups })
       }
     })
   }
@@ -95,7 +96,7 @@ apiGroups.getAll = function (req, res) {
   GroupSchema.getAllGroups(function (err, groups) {
     if (err) return res.status(400).json({ success: false, error: err.message })
 
-    return res.json({ success: true, groups: groups })
+    return res.json({ success: true, groups })
   })
 }
 
@@ -118,13 +119,13 @@ apiGroups.getAll = function (req, res) {
  *
  */
 apiGroups.getSingleGroup = function (req, res) {
-  var id = req.params.id
+  const id = req.params.id
   if (_.isUndefined(id)) return res.status(400).json({ error: 'Invalid Request' })
 
   GroupSchema.getGroupById(id, function (err, group) {
     if (err) return res.status(400).json({ error: err.message })
 
-    return res.status(200).json({ success: true, group: group })
+    return res.status(200).json({ success: true, group })
   })
 }
 
@@ -162,7 +163,7 @@ apiGroups.getSingleGroup = function (req, res) {
  }
  */
 apiGroups.create = function (req, res) {
-  var Group = new GroupSchema()
+  const Group = new GroupSchema()
 
   Group.name = req.body.name
   Group.members = req.body.members
@@ -171,7 +172,7 @@ apiGroups.create = function (req, res) {
   Group.save(function (err, group) {
     if (err) return res.status(400).json({ success: false, error: 'Error: ' + err.message })
 
-    res.json({ success: true, error: null, group: group })
+    res.json({ success: true, error: null, group })
   })
 }
 
@@ -209,8 +210,8 @@ apiGroups.create = function (req, res) {
  }
  */
 apiGroups.updateGroup = function (req, res) {
-  var id = req.params.id
-  var data = req.body
+  const id = req.params.id
+  const data = req.body
   if (_.isUndefined(id) || _.isUndefined(data) || !_.isObject(data))
     return res.status(400).json({ error: 'Invalid Post Data' })
 
@@ -224,8 +225,8 @@ apiGroups.updateGroup = function (req, res) {
   GroupSchema.getGroupById(id, function (err, group) {
     if (err) return res.status(400).json({ error: err.message })
 
-    var members = _.compact(data.members)
-    var sendMailTo = _.compact(data.sendMailTo)
+    const members = _.compact(data.members)
+    const sendMailTo = _.compact(data.sendMailTo)
 
     group.name = data.name
     group.members = members
@@ -234,6 +235,7 @@ apiGroups.updateGroup = function (req, res) {
     group.save(function (err, savedGroup) {
       if (err) return res.status(400).json({ error: err.message })
 
+      winston.info('The group was updated : ', JSON.stringify(savedGroup))
       return res.json({ success: true, group: savedGroup })
     })
   })
@@ -261,13 +263,13 @@ apiGroups.updateGroup = function (req, res) {
  }
  */
 apiGroups.deleteGroup = function (req, res) {
-  var id = req.params.id
+  const id = req.params.id
   if (_.isUndefined(id)) return res.status(400).json({ success: false, error: 'Error: Invalid Group Id.' })
 
   async.series(
     [
       function (next) {
-        var grps = [id]
+        const grps = [id]
         ticketSchema.getTickets(grps, function (err, tickets) {
           if (err) {
             return next('Error: ' + err.message)
